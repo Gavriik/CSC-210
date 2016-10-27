c3601cc06e0e5feccde381f5d9ba29c3460a4d24
Gabriel Espinoza <Halcyon@Gabriels-iMac.local>
2016-10-27 16:48:57 -0400

    updated classes

--- src/employeeworker6/ProductionWorker.java
+++ src/employeeworker6/ProductionWorker.java
@@ -1,5 +1,52 @@
 package employeeworker6;
 
-public class ProductionWorker {
+import java.text.DecimalFormat;
 
+public class ProductionWorker extends Employee {
+	public static final int DAY_SHIFT = 1;
+	public static final int NIGHT_SHIFT = 2;
+	private int shift;
+	private double payRate;
+
+	public ProductionWorker(String n, String num, String date, int sh, double rate) {
+		super(n, num, date);
+		shift = sh;
+		payRate = rate;
+	}
+
+	public ProductionWorker() {
+		super();
+		shift = DAY_SHIFT;
+		payRate = 0.0;
+	}
+	
+	public void setShift(int s) {
+		shift = s;
+	}
+
+	public void setPayRate(double p) {
+		payRate = p;
+	}
+
+	public int getShift() {
+		return shift;
+	}
+
+	public double getPayRate() {
+		return payRate;
+	}
+
+	public String toString() {
+		DecimalFormat dollar = new DecimalFormat("#.##0.00");
+		String str = super.toString();
+		str += "\nShift: ";
+		if (shift == DAY_SHIFT)
+			str += "Day";
+		else if (shift == NIGHT_SHIFT)
+			str += "Night";
+		else
+			str += "INVALID SHIFT NUMBERS";
+		return str += ("\nHourly Pay Rate: $" + dollar.format(payRate));
+	}
+	
 }
--- src/employeeworker6/ProductionWorkerDemo.java
+++ src/employeeworker6/ProductionWorkerDemo.java
@@ -1,10 +1,19 @@
 package employeeworker6;
 
 public class ProductionWorkerDemo {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ProductionWorker pw = new ProductionWorker("John Smith", "123-A", "11-15-2005", ProductionWorker.DAY_SHIFT,
+				16.50);
+		System.out.println("Here's the first production worker. ");
+		System.out.println(pw);
+		ProductionWorker pw2 = new ProductionWorker();
+		pw2.setName("Joan Jones");
+		pw2.setEmployeeNumber("222-L");
+		pw2.setHireDate("12-12-2005");
+		pw2.setShift(ProductionWorker.NIGHT_SHIFT);
+		pw2.setPayRate(18.50);
+		System.out.println("\nHere's the second production worker. ");
+		System.out.println(pw2);
 	}
-
+	
 }
